999b098992be7becfff91c9fa35267774b8009a9
Emerson Cavalcanti <emersoncavalcantijd@gmail.com>
2023-05-01 16:17:52 -0300

    finished splash

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutterando_mockup/pages/home_page.dart';
 
-import '../src/custoM_colors.dart';
+import '../src/config/custom_colors.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -11,11 +12,35 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const HomePage(),
+        ),
+      );
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Material(
       color: CustomColors.background,
       child: Center(
-        child: Image.asset('assets/images/masterclass_logo.png'),
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 71,
+            right: 70,
+          ),
+          child: SizedBox(
+            width: 287,
+            height: 75,
+            child: Image.asset('assets/images/masterclass_logo.png'),
+          ),
+        ),
       ),
     );
   }
